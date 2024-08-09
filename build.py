e30e0aa5768115d4f6293e0d77bdfa8f3903a12f
chentao <chentao@jscoe.com>
2024-08-09 13:48:22 +0800

    feat: add zip

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import sys
 import shutil
 import os
+import zipfile
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
@@ -13,9 +14,12 @@ current_resources_path = os.path.join(current_dir, 'resources')
 current_cfg_path = os.path.join(current_dir, 'gamestate_integration_d2gh.cfg')
 
 dist_dir = os.path.join(current_dir, 'dist')
+build_dir = os.path.join(current_dir, 'build')
 dist_icon_path = os.path.join(dist_dir, 'd2gh.ico')
 dist_resources_path = os.path.join(dist_dir, 'resources')
 dist_cfg_path = os.path.join(dist_dir, 'gamestate_integration_d2gh.cfg')
+
+dist_zip_path = os.path.join(current_dir, 'dist.zip')
 
 # copy to dist
 shutil.copy2(current_icon_path, dist_icon_path)
@@ -28,3 +32,16 @@ for item in os.listdir(current_resources_path):
     s = os.path.join(current_resources_path, item)
     d = os.path.join(dist_resources_path, item)
     shutil.copy2(s, d)
+
+# zip
+dist_zip = zipfile.ZipFile(dist_zip_path, "w", zipfile.ZIP_DEFLATED)
+for path, dir_names, filenames in os.walk(dist_dir):
+    fpath = path.replace(dist_dir, '')
+    for filename in filenames:
+        dist_zip.write(os.path.join(path, filename), os.path.join(fpath, filename))
+dist_zip.close()
+
+# delete
+shutil.rmtree(build_dir)
+shutil.rmtree(dist_dir)
+
